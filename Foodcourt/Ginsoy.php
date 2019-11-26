8a446dbf2fed2c8f258a8de1cad13da2167fde47
zobiasalman <45417354+zobiasalman@users.noreply.github.com>
2019-11-26 10:55:13 +0500

    Add files via upload

--- Foodcourt/Ginsoy.php
+++ Foodcourt/Ginsoy.php
@@ -17,54 +17,63 @@
 
 <?php
 
-$query = "select * from restaurant";
+$query = "select * from menu";
 
 mysqli_query($db, $query) or die('Error querying database.');
 
 $result = mysqli_query($db, $query);
 
 ?>
-       
+
+
 <section id="menu">
-        <h2 class="menu-title">Menu</h2>
+<table align = "center" border = "1px" style="width:600px; line-height:40px;">
+<tr>
+        <th colspan ="4"> <h2 class="menu-title">Menu</h2></th>
+</tr>
         <section class="tabs-container">
             <section class="tabs">
-                 <button class="tabHeader active-tab" data-tab = "5">ID</button>
-                 <button class="tabHeader" data-tab = "6">Deals</button>
-                 <button class="tabHeader" data-tab = "7">Deal_Price</button>
-                 <button class="tabHeader" data-tab = "8">Appetizer</button>
-                 <button class="tabHeader" data-tab = "9">Appetizer_Price</button>
-                 <button class="tabHeader" data-tab = "9">MainCourse</button>
-                 <button class="tabHeader" data-tab = "9">MainCourse_Price</button>
-                 <button class="tabHeader" data-tab = "9">Desserts</button>
-                 <button class="tabHeader" data-tab = "9">Desserts_Price</button>
-                 <button class="tabHeader" data-tab = "9">Beverages</button>
-                 <button class="tabHeader" data-tab = "9">Beverage_Price</button>
+     <t>
+               <th>  <button class="tabHeader active-tab" data-tab = "5">ID</button> </th>
+               <th>  <button class="tabHeader" data-tab = "6">Deals</button> </th>
+               <th>  <button class="tabHeader" data-tab = "7">Deal_Price</button> </th>
+               <th>  <button class="tabHeader" data-tab = "8">Appetizer</button> </th>
+               <th>  <button class="tabHeader" data-tab = "9">Appetizer_Price</button> </th>
+               <th>  <button class="tabHeader" data-tab = "9">MainCourse</button> </th>
+               <th>  <button class="tabHeader" data-tab = "9">MainCourse_Price</button> </th>
+               <th>  <button class="tabHeader" data-tab = "9">Desserts</button> </th>
+               <th>  <button class="tabHeader" data-tab = "9">Desserts_Price</button> </th>
+               <th>  <button class="tabHeader" data-tab = "9">Beverages</button> </th>
+               <th>  <button class="tabHeader" data-tab = "9">Beverage_Price</button> </th>
 
             </section>
+</t>
 
 <?php
-            while ($rows = mysqli_fetch_assoc($result)) {
+           while ($row = mysqli_fetch_array($result)) {
+          ?>
+          <tr>
+          <td><?php echo $row['ID']; ?></td>
+          <td><?php echo $row['Deals']; ?></td> 
+          <td><?php echo $row['Deal_Price']; ?></td> 
+          <td><?php echo $row['Appetizer'] ;?></td> 
+          <td><?php echo $row['Appetizer_Price'] ;?></td> 
+          <td><?php echo $row['MainCourse'] ;?></td> 
+          <td><?php echo $row['MainCourse_Price']; ?></td> 
+          <td><?php echo $row['Desserts'];?></td> 
+          <td><?php echo $row['Desserts_Price']; ?></td> 
+          <td><?php echo $row['Beverages']; ?></td> 
+          <td><?php echo $row['Beverage_Price']; ?> </td>
+          </tr>  
+<?php  
+          }
+      
     ?>
-
-    <?php echo $row['ID']; ?>
-    <?php echo $row['Deals']; ?>
-    <?php echo $row['Deal_Price']; ?>
-    <?php echo $row['Appetizer']; ?>
-    <?php echo $row['Appetizer_Price']; ?>
-    <?php echo $row['MainCourse']; ?>
-    <?php echo $row['MainCourse_Price']; ?>
-    <?php echo $row['Desserts']; ?>
-    <?php echo $row['Desserts_Price']; ?>
-    <?php echo $row['Beverages']; ?>
-    <?php echo $row['Beverage_Price']; ?>
-
-    <?php
-   }
-   ?>
+</table>
+    
 
 <script src="main.js"></script>
-    <script src="tabs.js"></script>
+<script src="tabs.js"></script>
 
 </body>
 </html>
